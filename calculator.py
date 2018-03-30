bd975e501bbe43aa079c5ddc86375a188d37abd7
caichang01 <cmx_as@hotmail.com>
2018-03-30 16:55:44 +0800

    使用namedtuple改写计算器程序

--- calculator.py
+++ calculator.py
@@ -1,37 +1,60 @@
 #！ -*- coding: utf-8 -*-
 import sys
 import csv
+from collections import namedtuple
 
-def TaxCalculate(name, tax_income):
-	if tax_income < 0:
-		income = 0
-	else:
-		income = tax_income
-	if income  <= 1500:
-		tax = (income * 0.03) - 0
-	elif income <= 4500:
-		tax = (income * 0.10) - 105
-	elif income <= 9000:
-		tax = (income * 0.20) - 555
-	elif income <= 35000:
-		tax = (income * 0.25) - 1005
-	elif income <= 55000:
-		tax = (income * 0.30) - 2755
-	elif income <= 80000:
-		tax = (income * 0.35) - 5505
-	else:  
-		tax = (income * 0.45) - 13505
-	sallary = format(tax_income + 3500 -tax, ".2f")
-	str1 = name + ':' + sallary
-	print(str1)
+#使用namedtuple存储个税速查表，包含三个属性，应纳税所得额（门槛），税率，速算扣除数
+TaxLookupItem =  namedtuple(
+	'TaxLookupItem',
+	['StartPoint','TaxRate','QuickDeduction']
+)
 
-try: 
-	for arg in sys.argv[1:]:
-		params = arg.split(':')
-		name = params[0]
-		income = int(params[1])
-		tax_income = (income * (1 - 0.08 - 0.02 - 0.005 - 0.06)) -3500
-		TaxCalculate(name, tax_income)
+#个税起征点3500
+TaxStartPoint = 3500
 
-except:
-	print("Parameter Error")
+#存储个税速查表
+IncomeTaxLookupTable = [
+	TaxLookupItem(80000, 0.45, 13505),
+	TaxLookupItem(55000, 0.35, 5505),
+	TaxLookupItem(35000, 0.30, 2755),
+	TaxLookupItem(9000, 0.25, 1005),
+	TaxLookupItem(4500, 0.20, 555),
+	TaxLookupItem(1500, 0.10, 105),
+	TaxLookupItem(1500, 0.3, 0),
+]
+
+#处理命令行参数类
+class Args(object):
+
+	#初始化时读取命令行所有参数到self.args列表中
+	def __init__(self):
+		self.args = sys.argv[1:]
+
+	#提取参数-c,-d,-o后面的值
+	def _value_after_option(self, option):
+		try:
+			#首先获得参数的索引值：
+			index = self.args.index(option)
+			#参数所在位置下一个的字符串就是所需的文件路径
+			return self.args[index + 1]
+		except (ValueError, IndexError):
+			#如果获取出错，自动打印错误信息并退出
+			print('Parameter Error')
+			exit()
+	
+	#获取 -c 参数对应的值，即配置文件的路径
+	@property
+	def config_path(self):
+		return self._value_after_option('-c')
+	
+	#获取 -d 参数对应的值，即用户数据文件的路径
+	@property
+	def userdata_path(self):
+		return self._value_after_option('-d')
+
+	#获取 -o 参数对应的值，即输出的用户工资单文件的路径
+	@property
+	def export_path(self):
+		return self._value_after_option('-o')
+
+	
