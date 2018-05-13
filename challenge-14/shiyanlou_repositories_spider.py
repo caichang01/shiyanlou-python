98a5f282531fc4913ba9d816e5f55aa142174829
caichang01 <cmx_as@hotmail.com>
2018-05-13 15:33:42 +0800

    增加挑战14实现

--- challenge-14/shiyanlou_repositories_spider.py
+++ challenge-14/shiyanlou_repositories_spider.py
@@ -10,7 +10,8 @@ class ShiyanlouRepositoriesSpider(scrapy.Spider):
         return (url_templ.format(i) for i in range(1, 5))
     
     def parse(self, response):
-        for repository in response.css('div#user-repositories-list'):
+        for repository in response.css('div#user-repositories-list > ul > li'):
             yield{
-                'name': repository.css('div#user-repositories-list > ul > li:nth-child(1) > div.d-inline-block.mb-1 h3::text').extract_first()
+                'name': repository.css('div.d-inline-block.mb-1 h3 a::text').re_first(r'[^\S]*(.+)[^\S]*'),
+                'updated_time': repository.css('div.f6.text-gray.mt-2 relative-time::attr(datetime)').extract_first()
             }
