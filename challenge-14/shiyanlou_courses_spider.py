985662573c8ee85daad26912d85b3f987ff71b0c
caichang01 <cmx_as@hotmail.com>
2018-05-13 14:45:28 +0800

    修复pylint告警提示

--- challenge-14/shiyanlou_courses_spider.py
+++ challenge-14/shiyanlou_courses_spider.py
@@ -21,5 +21,5 @@ class ShiyanlouCoursesSpider(scrapy.Spider):
                   'name': course.css('div.course-name::text').extract_first(),
                   'description': course.css('div.course-desc::text').extract_first(),
                   'type': course.css('div.course-footer span.pull-right::text').extract_first(default='Free'),
-                  'students': course.xpath('.//span[contains(@class, "pull-left")]/text()[2]').re_first('[^\d]*(\d+)[^\d]*')
+                  'students': course.xpath('.//span[contains(@class, "pull-left")]/text()[2]').re_first(r'[^\d]*(\d+)[^\d]*')
             }
