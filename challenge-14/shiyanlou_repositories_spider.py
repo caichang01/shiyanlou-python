4b79565f6190af288c038229a822de46fec46774
caichang01 <cmx_as@hotmail.com>
2018-05-14 11:18:55 +0800

    修复单行代码过长的问题

--- challenge-14/shiyanlou_repositories_spider.py
+++ challenge-14/shiyanlou_repositories_spider.py
@@ -12,6 +12,8 @@ class ShiyanlouRepositoriesSpider(scrapy.Spider):
     def parse(self, response):
         for repository in response.css('div#user-repositories-list > ul > li'):
             yield{
-                'name': repository.css('div.d-inline-block.mb-1 h3 a::text').re_first(r'[^\S]*(.+)[^\S]*'),
-                'updated_time': repository.css('div.f6.text-gray.mt-2 relative-time::attr(datetime)').extract_first()
+                'name': repository.css('div.d-inline-block.mb-1 h3 a::text')
+                .re_first(r'[^\S]*(.+)[^\S]*'),
+                'updated_time': repository.css('div.f6.text-gray.mt-2 \
+                relative-time::attr(datetime)').extract_first()
             }
