2c69384de73d0fad02ed5d228565ff0b2e4cbb1b
caichang01 <cmx_as@hotmail.com>
2018-07-21 06:47:14 +0800

    更改挑战24代码变量命名

--- challenge-24/apple_analysis.py
+++ challenge-24/apple_analysis.py
@@ -4,10 +4,10 @@ import pandas as pd
 def quarter_volume():
     data = pd.read_csv('apple.csv', header=0)
 
-    data_sum = data.Volume
-    data_sum.index = pd.to_datetime(data.Date)
+    data_serialized = data.Volume
+    data_serialized.index = pd.to_datetime(data.Date)
 
-    data_resample = data_sum.resample('Q').sum().sort_values()
+    data_resample = data_serialized.resample('Q').sum().sort_values()
     second_volume = data_resample[-2]
 
     # print(second_volume)
